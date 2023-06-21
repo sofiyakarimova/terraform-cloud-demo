8d674f3b4a5b805586a9662dc411d650612e65e4
Sofiya Karimova <skarimova1299@gmail.com>
2023-06-20 20:44:00 -0400

    added private subnets

---
+++ rds/main.tf
@@ -0,0 +1,16 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "sofsworkspace"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+resource "aws_db_subnet_group" "default" {
+  name       = "project-db-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
